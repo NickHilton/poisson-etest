ac946ed21ac35aa9b9f862b34a82872cf4a87aa8
NickHilton <32165552+NickHilton@users.noreply.github.com>
2020-09-30 17:12:04 -0700

    Fix setup to install transitive numpy (#1)

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, Extension
+from setuptools import dist
+dist.Distribution().fetch_build_eggs(['numpy>=1.17.0,<2'])
 
 long_description = """
 A numpy binding for the Poisson E-Test, described in this paper:
@@ -12,18 +13,21 @@ or via this link:
 http://www.ucs.louisiana.edu/~kxk4695/statcalc/pois2pval.for
 """
 
-fortran_ext = Extension(
-    "poisson_etest.poisson_etest_fortran", sources=["lib/poisson_etest.f"]
-)
+if __name__ == "__main__":
+    from numpy.distutils.core import setup, Extension
+    fortran_ext = Extension(
+        "poisson_etest.poisson_etest_fortran", sources=["lib/poisson_etest.f"]
+    )
 
-setup(
-    name="poisson_etest",
-    version="0.1",
-    url="https://github.com/NickHilton/poisson-etest",
-    packages=["poisson_etest"],
-    description="A poisson e-test.",
-    long_description=long_description,
-    ext_modules=[fortran_ext],
-    package_dir={"poisson_etest": "lib"},
-    install_requires=["numpy>=1.17.0,<2"]
-)
+    setup(
+        name="poisson_etest",
+        version="0.1",
+        url="https://github.com/NickHilton/poisson-etest",
+        packages=["poisson_etest"],
+        description="A poisson e-test.",
+        long_description=long_description,
+        ext_modules=[fortran_ext],
+        package_dir={"poisson_etest": "lib"},
+        setup_requires=["numpy>=1.17.0,<2"],
+        install_requires=["numpy>=1.17.0,<2"]
+    )
